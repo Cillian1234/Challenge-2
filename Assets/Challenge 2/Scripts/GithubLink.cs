8796c6f3671dc72476a608156c600ab6d9f75cef
Cillian <126096645+CillianSmee@users.noreply.github.com>
2023-10-09 22:35:07 +0100

    WebGl Stuff + link

---
+++ Assets/Challenge 2/Scripts/GithubLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubLink : MonoBehaviour
+{
+    public void openLink()
+    {
+        Application.OpenURL("https://github.com/Cillian1234/Challenge-2/tree/main");
+    }
+}
