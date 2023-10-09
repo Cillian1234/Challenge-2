6e8579ed29c26b01180a2ac67a4ac8da9a53d75e
Cillian <126096645+CillianSmee@users.noreply.github.com>
2023-10-09 22:18:32 +0100

    init commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool spawnDisabled = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spawnDisabled == false)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke("DisableSpawn", 0);
+            Invoke("EnableSpawn", 1);
+        }
+    }
+
+    void DisableSpawn()
+    {
+        spawnDisabled = true;
+    }
+    void EnableSpawn()
+    {
+        spawnDisabled = false;
+    }
+}
